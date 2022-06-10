408845a2b9856974ffdcc024a02bff9128eb3457
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-10 23:49:21 +0700

    update: setup chakra ui

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./styles/global.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
