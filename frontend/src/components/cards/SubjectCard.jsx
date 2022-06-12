41c47906f9ad2f355735f8fbd0d84d533c708a18
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-13 02:41:25 +0700

    test: serve fake data with json-server

--- frontend/src/components/cards/SubjectCard.jsx
+++ frontend/src/components/cards/SubjectCard.jsx
@@ -1,21 +1,27 @@
 import { Box, Button, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-export default function SubjectCard() {
+export default function SubjectCard({ id, thumbnail, title, title_eng, description }) {
   return (
     <>
-      <Box width="400px" bgColor="white" boxShadow="lg" borderRadius={8} p={8}>
+      <Box width="400px" minH="520px" bgColor="white" boxShadow="lg" borderRadius={8} p={8}>
         <VStack w="full" textAlign="left" spacing={6}>
-          <Image src="https://picsum.photos/600/300" borderRadius={4} />
-          <Heading as="h5" fontSize="2xl">
-            Teknologi Informasi (Information Technology)
-          </Heading>
-          <Text>
-            adalah jurusan yang mempelajari tentang sistem atau teknologi olah data termasuk implementasi pada...
-          </Text>
+          <Image src={thumbnail} alt={`Image of ${title_eng}`} borderRadius={4} />
+          <Box width="full">
+            <Heading as="h5" fontSize="2xl">
+              {title}
+            </Heading>
+            <Heading as="h5" fontSize="2xl" pt={2}>
+              ({title_eng})
+            </Heading>
+          </Box>
+
+          <Box height="100px">
+            <Text>{description}</Text>
+          </Box>
 
           <Box pt={4}>
-            <Button colorScheme="blue" variant="ghost">
+            <Button id={id} colorScheme="blue" variant="ghost">
               Learn More
             </Button>
           </Box>
--- frontend/src/components/sections/Learning.jsx
+++ frontend/src/components/sections/Learning.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 import { Box, Container, Flex, Heading, HStack, Stack, Text } from "@chakra-ui/react";
 import SubjectCard from "../cards/SubjectCard";
 
 export default function Learning() {
+  const url = "http://localhost:8000/material";
+
+  const [material, setMaterial] = useState(null);
+
+  useEffect(() => {
+    // nanti di improve lagi pakai axios
+    fetch(url).then((res) => {
+      return res.json().then((data) => {
+        // console.log(data);
+        setMaterial(data);
+      });
+    });
+  }, []);
+
   return (
     <>
       <Box as="section">
@@ -12,17 +28,24 @@ export default function Learning() {
                 What will you learn today?
               </Heading>
               <Text fontSize="3xl" opacity={0.9} mt={8}>
-                As a beginner, Future Map updates with your learning needs. Here we provide the latest updates on your
-                learning map.
+                As a beginner, Future Map updates with your learning needs. Here we provide the
+                latest updates on your learning map.
               </Text>
             </Box>
 
             <Flex width="50%" overflowX="scroll">
               <HStack justify="space-between" py={16} spacing={8}>
-                <SubjectCard />
-                <SubjectCard />
-                <SubjectCard />
-                <SubjectCard />
+                {material &&
+                  material.map((data) => (
+                    <SubjectCard
+                      key={data.id}
+                      id={data.id}
+                      thumbnail={data.thumbnail}
+                      title={data.title}
+                      title_eng={data.title_eng}
+                      description={data.description}
+                    />
+                  ))}
               </HStack>
             </Flex>
           </Stack>
