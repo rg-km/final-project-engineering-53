3ced13d8b935519fe68c28c7cad57eb3183f2cb8
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-13 12:00:36 +0700

    update: implement react-router dom v6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
-import Hero from "./components/sections/Hero";
-import Learning from "./components/sections/Learning";
+import Home from "./pages/Home";
+import Material from "./pages/Material";
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <main>
-        <Hero />
-        <Learning />
-      </main>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/material" element={<Material />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ frontend/src/pages/Material.js
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+export default function Material() {
+  return (
+    <Container maxW="container.xl">
+      <Heading as="h1" mt={8}>
+        Material
+      </Heading>
+    </Container>
+  );
+}
