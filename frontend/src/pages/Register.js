d96ca9879170e1e7ebc214a4b245bb16f70ba32f
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-22 09:39:36 +0700

    fix: Login and Register

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,103 +1,90 @@
-import { useState } from "react";
-// import axios from "axios";
-import axios from "../api/axios";
 import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Input,
+  Stack,
   FormControl,
   FormLabel,
-  Box,
-  Container,
-  Flex,
-  Input,
-  Heading,
-  Button,
-  Center,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
-
-const REGISTER_URL = "/register";
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/layouts/AuthLayout";
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        REGISTER_URL,
-        JSON.stringify({ email, password, username }),
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      );
-      console.log(response.data);
-      //
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <>
-      <Box as="section" bgColor="gray.400">
-        <Container maxW="container.xl" h="90vh" color="#1a202c">
-          <Flex w="full" h="full" direction="column" justify="center" align="center">
-            <Heading mb={6}>Register</Heading>
-            <Box
-              w="640px"
-              h="fit-content"
-              border="1px solid"
-              borderColor="gray.200"
-              bgColor="#f8f7fc"
-              borderRadius={8}
-              boxShadow="lg"
-              p={12}
+    <AuthLayout>
+      <Box
+        minW="720px"
+        minH="656px"
+        h="fit-content"
+        backgroundColor="white"
+        alignItems="center"
+        boxShadow="lg"
+        borderRadius={16}
+        p={16}
+        mt={24}
+      >
+        <form action="submit">
+          <Stack spacing={3}>
+            <Heading mb={6} color="black">
+              Register
+            </Heading>
+            <FormControl isRequired>
+              <FormLabel type="username" textColor="black">
+                Username:
+              </FormLabel>
+              <Input
+                id="username"
+                type="username"
+                aria-label="username"
+                borderRadius="50"
+                mb="10"
+                color="black"
+              />
+              <FormLabel type="email" textColor="black">
+                Email:
+              </FormLabel>
+              <Input
+                id="email"
+                type="email"
+                aria-label="email"
+                borderRadius="50"
+                mb="10"
+                color="black"
+              />
+              <FormLabel type="password" textColor="black">
+                Password:
+              </FormLabel>
+              <Input
+                id="password"
+                type="password"
+                aria-label="password"
+                borderRadius="50"
+                color="black"
+                mb={16}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              color="white"
+              bgColor="#2477FF"
+              colorScheme="blue"
+              size="md"
+              borderRadius="50"
             >
-              <form onSubmit={handleSubmit}>
-                <FormControl>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    borderColor="gray.400"
-                    isRequired
-                  />
-
-                  <FormLabel htmlFor="username" mt={6}>
-                    Username
-                  </FormLabel>
-                  <Input
-                    id="username"
-                    type="text"
-                    onChange={(e) => setUsername(e.target.value)}
-                    borderColor="gray.400"
-                    isRequired
-                  />
-
-                  <FormLabel htmlFor="password" mt={6}>
-                    Password
-                  </FormLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    borderColor="gray.400"
-                    isRequired
-                  />
-
-                  <Center>
-                    <Button mt={12} colorScheme="teal" type="submit">
-                      Submit
-                    </Button>
-                  </Center>
-                </FormControl>
-              </form>
-            </Box>
-          </Flex>
-        </Container>
+              Register
+            </Button>
+            <Text textAlign="center" fontWeight="medium">
+              Already have an account?{" "}
+              <ChakraLink as={Link} to="/login" fontWeight="semibold">
+                Login
+              </ChakraLink>
+            </Text>
+          </Stack>
+        </form>
       </Box>
-    </>
+    </AuthLayout>
   );
 }
