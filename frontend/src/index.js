5275408360e30ab46a89787f4644a501d4bae944
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-23 12:19:51 +0700

    add: user login using context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
