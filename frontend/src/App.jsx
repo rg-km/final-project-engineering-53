ccad779743b39d07c7559c060e00bcdfdd5b8b5d
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-27 01:51:30 +0700

    fix: define new routes and layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./helpers/RequireAuth";
-import AppLayout from "./components/layouts/AppLayout";
+import UserLayout from "./components/layouts/UserLayout";
 
 import NotFound from "./pages/404";
 
@@ -15,11 +15,13 @@ import Register from "./pages/Register";
 import AdminRegister from "./pages/admin/AdminRegister";
 import AdminLogin from "./pages/admin/AdminLogin";
 import Dashboard from "./pages/admin/Dashboard";
+import Profile from "./pages/user/Profile";
+import AdminHome from "./pages/admin/Home";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
+      <Route path="/" element={<UserLayout />}>
         {/* Public Routes  */}
         <Route path="/" element={<Home />} />
         <Route path="/materials">
@@ -27,21 +29,24 @@ export default function App() {
           <Route path=":id" element={<MaterialDetail />} />
         </Route>
 
+        {/* User Routes  */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Admin Routes  */}
-        <Route path="/admin/register" element={<AdminRegister />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-
-        {/* Protected Routes  */}
-        <Route element={<RequireAuth allowedRoles="admin" />}>
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-        </Route>
-
-        {/* Find The Missing Routes  */}
-        <Route path="*" element={<NotFound />} />
+        <Route path="/profile" element={<Profile />} />
       </Route>
+
+      {/* Admin Routes  */}
+      <Route path="/admin/register" element={<AdminRegister />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/home" element={<AdminHome />} />
+
+      {/* Protected Routes  */}
+      <Route element={<RequireAuth allowedRoles="admin" />}>
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+      </Route>
+
+      {/* Find The Missing Routes  */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
